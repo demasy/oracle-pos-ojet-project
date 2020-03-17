0611493a15572bb2b534f0cb117c5003c7733ad1
Ahmed El-Demasy <domssa@live.com>
2020-03-17 20:25:28 +0300

    Store N Branch in accordion
    Store N Branch in accordion

--- OraclePOSApplication/src/js/viewModels/dashboard.js
+++ OraclePOSApplication/src/js/viewModels/dashboard.js
@@ -18,7 +18,7 @@ define(
     , 'ojs/ojinputtext'
     , 'ojs/ojdatetimepicker'
     , 'ojs/ojselectcombobox'
-    , 'ojs/ojselectsingle','ojs/ojradioset'
+    , 'ojs/ojselectsingle','ojs/ojradioset','ojs/ojaccordion'
 
   ]
   ,
