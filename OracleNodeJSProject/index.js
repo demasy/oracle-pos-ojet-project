a44df24649819cff6f621fa30267518f40623900
Ahmed El-Demasy <domssa@live.com>
2020-03-26 21:20:36 +0300

    Shutting down up the connection pool
    If you rerun and shut down the application again, you should see that the database module closes after the web server closes, but before the process exits.

--- OracleNodeJSProject/index.js
+++ OracleNodeJSProject/index.js
@@ -1,8 +1,25 @@
 const webServer = require('./services/web-server.js');
+const dbConfig = require('./config/database.js');
+const database = require('./services/database.js');
 
+const defaultThreadPoolSize = 4;
+
+// Increase thread pool size by poolMax
+process.env.UV_THREADPOOL_SIZE = dbConfig.posPool.poolMax + defaultThreadPoolSize;
 
 async function startup() {
     console.log('Starting application');
+
+    try {
+        console.log('Initializing database module');
+
+        await database.initialize();
+    } catch (err) {
+        console.error(err);
+
+        process.exit(1); // Non-zero failure code
+    }
+
 
     try {
         console.log('Initializing web server module');
@@ -28,6 +45,16 @@ async function shutdown(e) {
 
         await webServer.close();
     } catch (e) {
+        console.log('Encountered error', e);
+
+        err = err || e;
+    }
+
+    try {
+        console.log('Closing database module');
+
+        await database.close();
+    } catch (err) {
         console.log('Encountered error', e);
 
         err = err || e;
--- OracleNodeJSProject/services/database.js
+++ OracleNodeJSProject/services/database.js
@@ -2,7 +2,7 @@ const oracledb = require('oracledb');
 const dbConfig = require('../config/database.js');
 
 async function initialize() {
-  await oracledb.createPool(dbConfig.posPool);
+  const pool = await oracledb.createPool(dbConfig.posPool);
 }
 
 module.exports.initialize = initialize;
@@ -12,32 +12,3 @@ async function close() {
 }
 
 module.exports.close = close;
-
-function simpleExecute(statement, binds = [], opts = {}) {
-  return new Promise(async (resolve, reject) => {
-    let conn;
-
-    opts.outFormat = oracledb.OBJECT;
-    opts.autoCommit = true;
-
-    try {
-      conn = await oracledb.getConnection();
-
-      const result = await conn.execute(statement, binds, opts);
-
-      resolve(result);
-    } catch (err) {
-      reject(err);
-    } finally {
-      if (conn) { // conn assignment worked, need to close
-        try {
-          await conn.close();
-        } catch (err) {
-          console.log(err);
-        }
-      }
-    }
-  });
-}
-
-module.exports.simpleExecute = simpleExecute;
